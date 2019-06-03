9c25f8f4e9d567fd4aebbdde084589ac31c997c7
Julie <39500350+JGD625@users.noreply.github.com>
2019-06-03 09:35:03 -0400

    Update app.js
    adjusted for heroku error

--- src/app.js
+++ src/app.js
@@ -4,45 +4,25 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const { API_TOKEN } = require('./config')
-const logger = require('./logger')
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
 const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
 app.use(helmet())
-app.use(express.json());
-
-app.use(function validateBearerToken(req, res, next) {
-  const authToken = req.get('Authorization')
-  logger.error(`Unauthorized request to path: ${req.path}`)
-
-  if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
-    return res.status(401).json({ error: 'Unauthorized request' })
-  }
-
-  next()
-})
+app.use(validateBearerToken)
 
 app.use(bookmarksRouter)
 
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-  } else {
-    console.error(error)
-    logger.error(error.message)
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response)
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
 })
 
+app.use(errorHandler)
 
 module.exports = app
